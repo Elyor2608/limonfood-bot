49149c79f304f84b7bc95a99e56f5a71cf5ccbf4
Elyor2608 <700elyor@gmail.com>
2026-05-05 15:59:58 +0500

    Update register.py

--- bot/states/register.py
+++ bot/states/register.py
@@ -1,15 +1,1 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class Register(StatesGroup):
-    telefon = State()
-    ism = State()
-
-class Order(StatesGroup):
-    restoran_tanlash = State()
-    mahsulot_tanlash = State()
-    manzil = State()
-    telefon = State()
-    tolav = State()
-
-class Location(StatesGroup):
-    yuborish = State()
+# states start.py ichida
