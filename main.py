3c69e6570f3f9e9408b39f0a16b622f5d5f3be82
Elyor2608 <700elyor@gmail.com>
2026-05-05 15:58:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import asyncio
 import os
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from bot.handlers import start
 from db.database import create_pool, create_tables
@@ -12,17 +12,16 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher(storage=MemoryStorage())
+bot = Bot(token=BOT_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-    dp.include_router(start.router)
+start.register_handlers(dp)
 
+async def on_startup(dp):
     await create_pool()
     await create_tables()
-
     print("LimonFood bot ishga tushdi! 🍋")
-    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
