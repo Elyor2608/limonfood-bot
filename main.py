3066ce2961d334bfbac3def766d35cd4e04d1140
Elyor2608 <700elyor@gmail.com>
2026-05-05 12:24:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-.
+import asyncio
+import os
+from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from bot.handlers import start
+from db.database import create_pool, create_tables
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.include_router(start.router)
+
+    await create_pool()
+    await create_tables()
+
+    print("LimonFood bot ishga tushdi! 🍋")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
